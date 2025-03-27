c5e1d7f6687418fdf63d467da5a295e6bdde676e
KevinMerino-Dev <k.merino.ing@gmail.com>
2025-03-27 15:35:28 -0300

    Sistema funcional del CityScopeJS

--- indicator-API/indicator_api.py
+++ indicator-API/indicator_api.py
@@ -1,59 +1,72 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Callable
 import os
 import json
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+
+#La clase indicador se encarga de contener los atributos y métodos necesarios para la creación de un indicador.
 class Indicator:
     def __init__(self):
-        self.data = None
+       #Contiene los datos de la capa actual
+        self.data = None 
+        #Contiene las capas de indicadores
         self.layers = []
+        #Contiene los indicadores numéricos
         self.numeric = []
+        #Contiene un diccionario con las capas de indicadores
         self.dict_of_layers = {}
+        #Contiene un diccionario con los indicadores numéricos
         self.dict_of_numeric = {}
+        #Contiene la capa actual
         self.actual_layer = None
+        #Contiene la columna de valores para el indicador
         self.value_column = None
+        #Contiene los valores mínimo y máximo para normalizar el colormap
         self.min_value = None
         self.max_value = None
+        #Contiene el colormap para el indicador
         self.colormap = plt.cm.jet
 
-    def create_layer(self, layer_id: str):
+    def create_layer(self, layer_id: str): #Crea una capa vacía con el ID especificado
         self.dict_of_layers[layer_id] = None
 
-    def set_layer_to_edit(self, layer_id: str):
+    def set_layer_to_edit(self, layer_id: str): #Establece la capa actual para editarla
         if layer_id not in self.dict_of_layers:
             raise ValueError(f"Layer '{layer_id}' not found. Load the layer data first.")
         self.actual_layer = layer_id
         self.data = None
 
-    def read_data_to_layer(self, filepath: str):
+    def read_data_to_layer(self, filepath: str): #Lee los datos de un archivo y los guarda en el diccionario de capas
         self.read_layer_data(filepath, self.actual_layer)
 
-    def read_layer_data(self, filepath: str, layer_id: str):
+    def read_layer_data(self, filepath: str, layer_id: str): #Lee los datos de un archivo y los guarda en un diccionario
         data = gpd.read_file(filepath)
-        data = data.to_crs(epsg=4326)
+        data = data.to_crs(epsg=4326) #Se convierten los datos a EPSG 4326
         if 'geometry' not in data.columns:
             raise ValueError("GeoDataFrame must contain a 'geometry' column.")
         self.dict_of_layers[layer_id] = data
         self.data = data
 
-    def set_value_column(self, column_name: str):
+
+    def set_value_column(self, column_name: str): #Establece la columna de valores para el indicador
         if column_name not in self.data.columns:
             raise ValueError(f"Column '{column_name}' does not exist in the data.")
         self.value_column = column_name
 
-    def set_min_max(self, min_value: float, max_value: float):
+
+    def set_min_max(self, min_value: float, max_value: float):#Establece los valores mínimo y máximo para normalizar el colormap
         self.min_value = min_value
         self.max_value = max_value
 
-    def set_colormap(self, colormap: Any):
+    def set_colormap(self, colormap: Any): #Establece el colormap para el indicador
         self.colormap = colormap
 
-    def add_fill_column(self):
+    def add_fill_column(self): #Agrega una columna de relleno a los datos, con esta nueva columna se  agregan los valores y colores a los polígonos.
         if self.value_column is None:
             raise ValueError("Value column must be set before calculating colors.")
         norm = plt.Normalize(vmin=self.min_value, vmax=self.max_value)
@@ -61,32 +74,65 @@ class Indicator:
         self.data['color'] = self.data['color'].apply(lambda rgb: tuple(int(c * 255) for c in rgb))
         self.data['fill'] = self.data['color'].apply(lambda x: '#%02x%02x%02x' % (x[0], x[1], x[2]))
 
-    def set_elevation_column(self, column_name: str):
+    def delete_column(self, column_name: str): #Elimina una columna de los datos
+        if self.value_column is None:
+            raise ValueError("Value column must be set before calculating colors.")
+        self.data.drop(columns=[column_name], inplace=True)
+
+    def set_elevation_column(self, column_name: str): #Establece la columna que contiene los valores de elevación para el indicador
         if column_name not in self.data.columns:
             raise ValueError(f"Column '{column_name}' does not exist in the data.")
         self.data['elevation'] = self.data[column_name]
 
-    def update_layer(self):
+    def update_layer(self): #Actualiza la capa actual con los datos actuales
         self.dict_of_layers[self.actual_layer] = self.data
 
-    def add_indicator_layer(self, layer_id: str, layer_type: str = "geojson"):
+    def add_indicator_layer(self, layer_id: str, layer_type: str ): #Agregar una capa de indicador a la lista de capas.
         data = self.dict_of_layers[layer_id]
         if data is None:
             raise ValueError("Data not loaded. Use read_data first.")
         features = json.loads(data.to_json())
         self.layers.append({"id": layer_id, "type": layer_type, "data": features, "properties": {"filled": True}})
 
-    def add_indicator_numeric(self, layer_id: str):
+    def add_indicator_numeric(self, layer_id: str): #Agrega un indicador numérico a la lista de indicadores numéricos
         self.numeric.append({"id": layer_id, "values": self.data[self.value_column].tolist()})
 
-    def clear_layers_and_numeric(self):
+    def clear_layers_and_numeric(self): #Limpia las listas de capas y de indicadores numéricos
         self.layers = []
         self.numeric = []
 
-    def get_layers_and_numeric(self) -> Dict[str, List[Any]]:
+    def get_layers_and_numeric(self) -> Dict[str, List[Any]]: #Devuelve un diccionario con las listas de capas e indicadores numéricos
         return {"layers": self.layers, "numeric": self.numeric}
+    
+    def delete_columns(self, columns_to_delete: list):
+        if self.data is None:
+            raise ValueError("No data loaded in the current layer.")
+        print("Columns before deleting:", self.data.columns.tolist())
+        self.data = self.data.drop(columns=columns_to_delete, errors='ignore')
+        if self.actual_layer:
+            self.dict_of_layers[self.actual_layer] = self.data
+        print("Columns after deleting:", self.data.columns.tolist())
+
+    def set_color_column(self, color_column: str): 
+        if self.data is None:
+            raise ValueError("No data loaded in the current layer.")
+        norm = plt.Normalize(vmin=self.min_value, vmax=self.max_value)
+        self.data[color_column] = self.data[self.value_column].apply(lambda x: self.colormap(norm(x))[:3])
+        self.data[color_column] = self.data[color_column].apply(lambda rgb: tuple(int(c * 255) for c in rgb))
+        #self.data['fill'] = self.data['color'].apply(lambda x: '#%02x%02x%02x' % (x[0], x[1], x[2]))
+
+    def set_key_column(self, key_column: str): 
+        if self.data is None:
+            raise ValueError("No data loaded in the current layer.")
+        if self.value_column is None:
+            raise ValueError("Value column must be set before calculating colors.")
+        self.data[key_column] = self.data[self.value_column]
+        #self.data[key_column] = self.data[self.value_column].apply(lambda x: x)
+        print(f"Key column '{key_column}' added successfully. Sample values:")
+        print(self.data[key_column].head())
 
 
+#A partir de ahora se comenzará a desarrollar la API REST con FastAPI.
 app = FastAPI()
 indicator = Indicator()
 
@@ -94,11 +140,13 @@ indicator = Indicator()
 os.makedirs("/app/data", exist_ok=True)
 app.mount("/static", StaticFiles(directory="/app/data"), name="static")
 
-class FileRequest(BaseModel):
+#FileRequest especifica el nombre del archivo a cargar.
+class FileRequest(BaseModel): 
     filename: str
 
+#Cargamos el archivo y lo almacenamos en una capa
 @app.post("/read_data")
-async def read_data(file_request: FileRequest):
+async def read_data(file_request: FileRequest): 
     try:
         filename = f"/app/data/{file_request.filename}"
         indicator.read_layer_data(filename, "default_layer")
@@ -108,10 +156,11 @@ async def read_data(file_request: FileRequest):
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-
+#LayerRequest especifica el ID o identificador de la capa a crear.
 class LayerRequest(BaseModel):
     layer_id: str
 
+#Creamos una capa vacía con el ID especificado.
 @app.post("/create_layer/")
 async def create_layer(request: LayerRequest):
     """
@@ -123,7 +172,19 @@ async def create_layer(request: LayerRequest):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+#Para configurar las columnas de valores.
+class ValueColumnRequest(BaseModel):
+    column_name: str
 
+@app.post("/set_value_column/")
+async def set_value_column(request: ValueColumnRequest):
+    try:
+        indicator.set_value_column(request.column_name)
+        return {"message": f"Value column '{request.column_name}' set successfully."}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+#Establecemos la capa actual para editarla.
 @app.post("/set_layer_to_edit/")
 async def set_layer_to_edit(request: LayerRequest):
     try:
@@ -133,9 +194,11 @@ async def set_layer_to_edit(request: LayerRequest):
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+#FilePathRequest especifica la ruta del archivo a cargar, basicamente maneja los indentificadores de las capas.
 class FilePathRequest(BaseModel):
     filepath: str
 
+#Leemos los datos de un archivo y los almacenamos en la capa actual.
 @app.post("/read_data_to_layer/")
 async def read_data_to_layer(request: FilePathRequest):
     try:
@@ -148,6 +211,7 @@ async def read_data_to_layer(request: FilePathRequest):
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="File not found.")
 
+
 @app.post("/update_layer/")
 async def update_layer():
     try:
@@ -156,11 +220,11 @@ async def update_layer():
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+#IndicatorLayerRequest especifica el ID de la capa a agregar como indicador.
 class IndicatorLayerRequest(BaseModel):
     layer_id: str
-    #layer_type: str .format= "geojson" #se cambio a la línea de abajo
-    layer_type: str = "geojson"
-    #layer_type: Literal["geojson", "parquet", "vector"]="geojson"
+    layer_type: str
+
 
 @app.post("/add_indicator_layer/")
 async def add_indicator_layer(request: IndicatorLayerRequest):
@@ -178,6 +242,7 @@ async def add_indicator_numeric(layer_id: str):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+#ElevationRequest especifica el nombre de la columna de elevación.
 class ElevationRequest(BaseModel):
     column_name: str
 
@@ -189,6 +254,7 @@ async def set_elevation(request: ElevationRequest):
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+#MinMaxRequest especifica los valores mínimo y máximo para normalizar el colormap.
 class MinMaxRequest(BaseModel):
     min_value: float
     max_value: float
@@ -204,6 +270,7 @@ async def set_min_max(request: MinMaxRequest):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+#ColormapRequest especifica el nombre del colormap a utilizar y configura el mapa de color para el indicador.
 class ColormapRequest(BaseModel):
     colormap_name: str
 
@@ -226,7 +293,7 @@ async def add_fill_column():
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-
+#Reiniciamos las capas e indicadores
 @app.post("/clear_layers_and_numeric/")
 async def clear_layers_and_numeric():
     try:
@@ -235,10 +302,58 @@ async def clear_layers_and_numeric():
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+# Modelo para recibir la lista de columnas a eliminar
+class DeleteColumnsRequest(BaseModel):
+    columns: list
 
+@app.post("/delete_columns/")
+async def delete_columns_endpoint(request: DeleteColumnsRequest):
+    try:
+        # Se asume que la capa a editar ya ha sido establecida
+        indicator.delete_columns(request.columns)
+        return {"message": "Columnas eliminadas correctamente.", "remaining_columns": indicator.data.columns.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+class columnRequest(BaseModel):
+    column: str
+
+@app.post("/add_color_column/")
+async def set_color_column(request: columnRequest):
+    try:
+        # Se asume que la capa a editar ya ha sido establecida
+        indicator.set_color_column(request.column)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.post("/add_key_column/")
+async def set_key_column(request: columnRequest):
+    try:
+        # Se asume que la capa a editar ya ha sido establecida
+        indicator.set_key_column(request.column)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+
+#Home
+@app.get("/")
+async def root():
+    return {"message": "Home - API de Indicadores"}
+
+#Recupera información sobre las capas e indicadores
 @app.get("/get_layers_and_numeric/")
 async def get_layers_and_numeric():
     try:
         return indicator.get_layers_and_numeric()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+#Recupera información sobre las capas e indicadores
+@app.get("/get_columns/")
+async def get_columns():
+    try:
+        return indicator.data.columns
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- scripts/MicroBrix.py
+++ scripts/MicroBrix.py
@@ -6,22 +6,24 @@ from threading import Thread
 
 class MicroBrix():
 
-    remote_host = 'cityio.media.mit.edu/cityio'
-    geogrid_data = {}
-    geogrid = {}
+    remote_host = 'cityio.media.mit.edu/cityio' #Se conecta al servidor CityIO.
+    geogrid_data = {}  #Es un diccionario para almacenar los datos del grid geográfico.
+    geogrid = {}  #Este diccionario almacena el grid estructural.
 
-    def __init__(self, table_name=None,
-            quietly=False,
-            host_mode ='remote',
-            host_name = None,
-            core = False,
-            core_name = None,
-            core_description = None,
-            core_category = None,
-            keep_updating = False,
-            update_interval = 5.0,
-            module_function = None,
-            save = False
+    #Este es el constructor!
+    def __init__(self, 
+            table_name=None,         #Nombre de la tabla a suscribirse
+            quietly=False,           
+            host_mode ='remote',     #Especifica la forma de conectarse. local o remote.
+            host_name = None,        
+            core = False,            #Indica si el módulo es "core" en CityIO
+            core_name = None,        
+            core_description = None, 
+            core_category = None,     
+            keep_updating = False,   #Permiten realizar actualizaciones automáticas
+            update_interval = 5.0,   #Intervalo en el que se realiza la actualización
+            module_function = None,  #Función que devuelve las capas (layers) y los datos (numeric)
+            save = False             #Este apartado icore_descriptionndica si los datos se guardarán en la tabla.
     ):
 
         if host_name is None:
@@ -30,6 +32,7 @@ class MicroBrix():
             self.host = host_name.strip('/')
         self.host = '127.0.0.1:8080' if host_mode=='local' else self.host
 
+        #Variables iniciales
         self.quietly = quietly
         self.save = save
         self.keep_updating = keep_updating
@@ -40,6 +43,7 @@ class MicroBrix():
         self.core_description = core_description
         self.core_category = core_category
         self.secure_protocol = '' if host_mode == 'local' else 's'
+        #Variables para definir URL para interactuar con la API de CityIO
         self.front_end_url   = f'http{self.secure_protocol}://cityio-beta.media.mit.edu/?cityscope={self.table_name}'
         self.cityIO_post_url = f'http{self.secure_protocol}://{self.host}/api/table/{table_name}/'
         self.cityIO_list = f'http{self.secure_protocol}://{self.host}/api/table/list/'
@@ -54,53 +58,58 @@ class MicroBrix():
 
         if(not self.quietly):
             websocket.enableTrace(True)
+        #Configuramos el WebSocket
         self.ws = websocket.WebSocketApp(
             self.cityIO_wss,
-            on_open=self.on_open,
-            on_message=self.on_message,
-            on_error=self.on_error,
-            on_close=self.on_close
+            on_open=self.on_open,   
+            on_message=self.on_message, #Manejo de mensajes recibidos
+            on_error=self.on_error,     #Captura los errores de la conexión
+            on_close=self.on_close     
             )
-
+        
+    #Manejo de Mensajes
     def on_message(self, ws, message):
-        dict_rec = json.loads(message)
-        message_type = dict_rec['type']
-        if(message_type == 'TABLE_SNAPSHOT'):
+        dict_rec = json.loads(message)  #Toma el mensaje que trae un JSON
+        message_type = dict_rec['type'] #Cambia el formato de json a diccionario de Python
+        if(message_type == 'TABLE_SNAPSHOT'):      #Table_snapshot
             table_name = dict_rec['content']['tableName']
             self.geogrid_data[table_name] = dict_rec['content']['snapshot']['GEOGRIDDATA']
             self.geogrid[table_name] = dict_rec['content']['snapshot']['GEOGRID']
             self.perform_update(table_name)
             thread = Thread(target = self.threaded_function, args = (table_name, ), daemon=True)
             thread.start()
-        elif(message_type == 'GEOGRIDDATA_UPDATE'):
+        elif(message_type == 'GEOGRIDDATA_UPDATE'):                 #Geogriddata
             print(dict_rec)
             table_name = dict_rec['content']['tableName']
             self.geogrid_data[table_name] = dict_rec['content']['geogriddata']
             self.perform_update(table_name)
-        elif(self.core and message_type == 'SUBSCRIPTION_REQUEST'):
+        elif(self.core and message_type == 'SUBSCRIPTION_REQUEST'):  #Subscription
             requester = dict_rec['content']['table']
             self.send_message(json.dumps({"type":"SUBSCRIBE","content":{"gridId":requester}}))
-        elif(self.core and message_type == 'SUBSCRIPTION_REMOVAL_REQUEST'):
-            requester = dict_rec['content']['table']
+        elif(self.core and message_type == 'SUBSCRIPTION_REMOVAL_REQUEST'):   #Removal
+            requester = dict_rec['content']['table']  
             # self._clear_values(requester)
             self.send_message(json.dumps({"type":"UNSUBSCRIBE","content":{"gridId":requester}}))
 
-    def on_error(self, ws, error):
+    def on_error(self, ws, error):   #Definimos errores
         print(error)
 
-    def on_close(self, ws, close_status_code, close_msg):
+    def on_close(self, ws, close_status_code, close_msg):   
         print("## Connection closed")
+   
 
-    def on_open(self, ws):
+    def on_open(self, ws):     
         print("## Opened connection")
         if self.core:
             self.send_message(json.dumps({"type":"CORE_MODULE_REGISTRATION","content":{"name":self.core_name, "description": self.core_description, "moduleType":self.core_category}}))
         else:
             self.send_message(json.dumps({"type":"SUBSCRIBE","content":{"gridId":self.table_name}}))
 
+    #Entrega el mensaje guardado en "message"
     def send_message(self, message):
         self.ws.send(message)
 
+    #Linea para actualizaciones
     def threaded_function(self,table_name):
         if(self.keep_updating):
             while True:
@@ -109,7 +118,9 @@ class MicroBrix():
                     self.perform_update(table_name)
                 except:
                     continue
-
+    
+    #Construye el mensaje con los datos
+    #Entrega Layers y Numeric de acuerdo a lo que se recibio
     def _send_indicators(self, layers, numeric, table):
         if(layers is not None and numeric is not None):
             message = {"type": "MODULE", "content":{"gridId": table, "save": self.save, "moduleData":{"layers":layers,"numeric":numeric}}}
@@ -119,15 +130,17 @@ class MicroBrix():
             message = {"type": "MODULE", "content":{"gridId": table, "save": self.save, "moduleData":{"numeric":numeric}}}
 
         self.send_message(json.dumps(message))
-
+    
+    #Llama al modulo para generar capas y los datos numericos. Esta información se envía mediante send_indicators.
     def perform_update(self,table):
         layers, numeric = self.module_function(self.geogrid[table],self.geogrid_data[table])
         self._send_indicators(layers, numeric ,table)
 
+    #Esta linea de codigo abre la comunicación. Abre el WebSocket
     def listen(self):
         self.ws.run_forever(dispatcher=rel, reconnect=5)
         rel.signal(2, rel.abort)  # Keyboard Interrupt
         rel.dispatch()
-
+    
     def stop(self):
         self.ws.close()
